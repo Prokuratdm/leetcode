07448883998e0c406a1660a2e968d97c9959b514
Dima <prokuratdm@gmail.com>
2024-06-21 16:18:15 +0300

    TASK 925

---
+++ src/main/java/org/example/gt900/T925.java
@@ -0,0 +1,54 @@
+package org.example.gt900;
+
+import java.util.LinkedList;
+
+public class T925 {
+
+    public boolean isLongPressedName(String name, String typed) {
+        LinkedList<LetterStat> statsName = createStats(name);
+        LinkedList<LetterStat> statsTyped = createStats(typed);
+
+        if (statsName.size() != statsTyped.size()) {
+            return false;
+        }
+
+        var ni=statsName.iterator();
+        var ti=statsTyped.iterator();
+
+        for (int i = 0; i < statsName.size(); i++) {
+            var nl=ni.next();
+            var tl=ti.next();
+            if(nl.letter!= tl.letter) return false;
+            if (nl.count > tl.count) return false;
+        }
+        return true;
+
+
+    }
+
+    private LinkedList<LetterStat> createStats(String name) {
+        LinkedList<LetterStat> statsName = new LinkedList<>();
+        for (char c : name.toCharArray()) {
+            if (statsName.isEmpty()) statsName.add(new LetterStat(c));
+            else if (statsName.getLast().letter == c) {
+                statsName.getLast().count++;
+            } else {
+                statsName.add(new LetterStat(c));
+            }
+
+        }
+        return statsName;
+    }
+
+    class LetterStat {
+        char letter;
+        int count;
+
+        public LetterStat(char letter) {
+            this.letter = letter;
+            count = 1;
+        }
+
+
+    }
+}
